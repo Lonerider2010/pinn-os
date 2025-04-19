2c0f644a0cfba16f0f53ca3f0de207e757f39a2a
Dietmar Schneider <Dietmar.Schneider@StartMail.com>
2025-04-19 22:37:30 +0200

    New calculation of partitions

--- DietPi_64/HOW-TO.sh
+++ DietPi_64/HOW-TO.sh
@@ -25,7 +25,12 @@ unxz ${dietpi_img}.xz
 
 fdisk -l ${dietpi_img}
 { read boot_size; read root_size; } <<<  "$(fdisk -l DietPi_RPi5-ARMv8-Bookworm.img -o Size | tail -2)"
-
+boot_size_v=${boot_size::-1}
+boot_size_i=$(echo $boot_size_v | cut -d "," -f1)
+boot_size_p="$(($boot_size_i + 100))"
+root_size_v=${root_size::-1}
+root_size_i=$(echo $root_size_v | cut -d "," -f1)
+root_size_p="$(($root_size_i * 2))"
 loop_dev=$(losetup --find --show --partscan DietPi_RPi5-ARMv8-Bookworm.img)
 
 # Boot tarball
@@ -39,13 +44,14 @@ xz -T1 -9 -e ${boot_tar} # T1 due to Raspi
 # Root tarball
 mount "${loop_dev}"p2 root
 r_uncompressed_tarball_size=$(du -h -m --max-depth=0 root | cut -f1)    #root uncompressed_tarball_size
+
 cd root || exit
 bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../${root_tar} .
 . boot/dietpi/.version   #version = {G_DIETPI_VERSION_CORE}.{G_DIETPI_VERSION_SUB}.{G_DIETPI_VERSION_RC}
 core=${G_DIETPI_VERSION_CORE}
 sub=${G_DIETPI_VERSION_SUB}
 rc=${G_DIETPI_VERSION_RC}
-version="${core}.${sub}.${rc}"
+version="v${core}.${sub}.${rc}"
 
 cd .. && umount root
 xz -T1 -9 -e ${root_tar} # T1 due to Raspi
@@ -78,22 +84,57 @@ image_string=$(jq -n --indent 4 \
     --argjson supports_backup "$supports_backup" \
     --argjson download_size "$download_size" \
     --argjson supported_models "$(jq -n '$ARGS.positional' --args "${supported_models[@]}")" \
-    '{name: $name, version: $version, release_date: $release_date, description: $description, url: $url, group: $group, username: $username, password: $password, supports_backup: $supports_backup, download_size: $download_size, supported_models: $supported_models}')
+    '{
+        name: $name,
+        version: $version,
+        release_date: $release_date,
+        description: $description,
+        url: $url,
+        group: $group,
+        username: $username,
+        password: $password,
+        supports_backup: $supports_backup,
+        download_size: $download_size,
+        supported_models: $supported_models
+      }')
 echo "$image_string" | tee os.json
 
 # Data for partitions.json
-echo -e "\nBoot partition\n=============="
-echo "label: boot"
-echo "filesystem_type: FAT"
-echo "partition_size_nominal: ${boot_size::-1}"
-echo "want_maximised: false"
-echo "uncompressed_tarball_size: ${b_uncompressed_tarball_size}"
-echo "sha512sum: ${b_sha512sum}"
-echo -e "\nRoot partition\n=============="
-echo "label: root"
-echo "filesystem_type: ext4"
-echo "partition_size_nominal: ${root_size::-1}"
-echo "want_maximised: true"
-echo "mkfs_options: -O ^huge_file"
-echo "uncompressed_tarball_size: ${r_uncompressed_tarball_size}"
-echo "sha512sum: ${r_sha512sum}"
+partition_string=$(jq -n --indent 2 \
+    --arg b_label "boot" \
+    --arg b_filesystem_type "FAT" \
+    --argjson b_partition_size_nominal "${boot_size_p}" \
+    --argjson b_want_maximised "false" \
+    --argjson b_uncompressed_tarball_size "$b_uncompressed_tarball_size" \
+    --arg b_sha512sum "$b_sha512sum" \
+    --arg r_label "root" \
+    --arg r_filesystem_type "ext4" \
+    --argjson r_partition_size_nominal "${root_size_p}" \
+    --argjson r_want_maximised "true" \
+    --arg r_mkfs_options "-O ^huge_file" \
+    --argjson r_uncompressed_tarball_size "${r_uncompressed_tarball_size}" \
+    --arg r_sha512sum "$r_sha512sum" \
+    '
+    {
+      "partitions": [
+        {
+          "label": $b_label,
+          "filesystem_type": $b_filesystem_type,
+          "partition_size_nominal": $b_partition_size_nominal,
+          "want_maximised": $b_want_maximised,
+          "uncompressed_tarball_size": $b_uncompressed_tarball_size,
+          "sha512sum": $b_sha512sum
+        },
+        {
+          "label": $r_label,
+          "filesystem_type": $r_filesystem_type,
+          "partition_size_nominal": $r_partition_size_nominal,
+          "want_maximised": $r_want_maximised,
+          "mkfs_options": $r_mkfs_options,
+          "uncompressed_tarball_size": $r_uncompressed_tarball_size,
+          "sha512sum": $r_sha512sum
+        }
+      ]
+    }
+    ')
+echo "$partition_string" | tee partitions.json
